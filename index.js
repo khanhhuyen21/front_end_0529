182eafa14cc70199a5dc13e2431506d3f248af21
khanhhuyen21 <huyensan21@gmail.com>
2023-06-12 15:17:53 +0700

    clone

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-arr = [2, 1, 6, 9];
+// arr = [2, 1, 6, 9];
 
-tìm số chẵn => chia hết cho 2;
-sum = 0;
-sum = sum + 2; sum = 2;
-sum = sum + 6; sum = 8;
-i : số lần lặp;
-i = 0;
-i < 4;
-i = i + 1;
+// tìm số chẵn => chia hết cho 2;
+// sum = 0;
+// sum = sum + 2; sum = 2;
+// sum = sum + 6; sum = 8;
+// i : số lần lặp;
+// i = 0;
+// i < 4;
+// i = i + 1;
 
-i = 1;
-i = i + 1; => i = 2;
-i = i + 1; => i =3;
-i = i + 1 => i = 4;
+// i = 1;
+// i = i + 1; => i = 2;
+// i = i + 1; => i =3;
+// i = i + 1 => i = 4;
 
-số lẻ =>
+// số lẻ =>
+
+// hello
